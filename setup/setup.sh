d1dfa5fb148f0992533846c86224040e62d97022
John Goerzen <jgoerzen@complete.org>
2017-05-02 00:53:44 -0500

    remove more refs to dosbox

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,16 +16,6 @@ mkdir /dos/drive_y/SCRIPTS
 cd /tmp/setup
 zcat freedos-c.qcow2.gz > /dos/freedos-c.qcow2
 
-DOSBOXCONF="`dosbox -printconf`"
-mv "$DOSBOXCONF" /dos/dosbox.conf
-for ASDF in c d e f g h y; do 
-  echo "mount $ASDF /dos/drive_$ASDF" >> /dos/dosbox.conf
-done
-echo 'path %PATH%;Y:\DOS;Y:\SCRIPTS' >> /dos/dosbox.conf
-
-# Fix an issue with the VNC console
-sed -i 's/usescancodes=true/usescancodes=false/' /dos/dosbox.conf
-
 # Convenience aliases
 cd /usr/local/bin
 ln -s /usr/bin/vim.tiny vim
